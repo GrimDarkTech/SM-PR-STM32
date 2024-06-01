6eb312a14a5c1db3710a2d3954c59997b5e86371
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-06-01 06:52:16 +0400

    #Stepper fixed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,15 +23,15 @@ int main(void)
 //  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
 //  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
 
-  int encoderA = 0;
-  int encoderB = 0;
+  int encoderA = -1;
+  int encoderB = -1;
 
-  int prevA = 0;
-  int prevB = 0;
+  int prevA = -1;
+  int prevB = -1;
 
   int dutyCycle = 0;
 
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+//  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 
   while (1)
   {
@@ -93,23 +93,47 @@ int main(void)
 	  }
 	  else
 	  {
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, encoderA);
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, encoderB);
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, prevA);
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, prevB);
+		  if(value < 0)
+		  {
+			  value = 50;
+		  }
 
-		   while(dutyCycle < 65535)
-		        {
-		            TIM1->CCR1 = dutyCycle;
-		            dutyCycle += 100;
-		            HAL_Delay(1);
-		        }
-		        while(dutyCycle > 0)
-		        {
-		            TIM1->CCR1 = dutyCycle;
-		            dutyCycle -= 100;
-		            HAL_Delay(1);
-		        }
+		  while(value > 0)
+		  {
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 1);
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+				  HAL_Delay(5);
+
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 0);
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 1);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+				  HAL_Delay(5);
+
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 0);
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 1);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 1);
+				  HAL_Delay(5);
+
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 1);
+				  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 1);
+				  HAL_Delay(5);
+
+			  value--;
+
+			  WriteInt2Bits(value);
+		  }
+
+		  HAL_Delay(100);
+		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 0);
+		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
 	  }
 
 
@@ -186,14 +210,6 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_TIM1_CLK_ENABLE();
 
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
-
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
 // Read
 
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 0);
@@ -262,7 +278,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
 
   GPIO_InitStruct.Pin = GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -270,53 +286,92 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, 0);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, 0);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 0);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
 static void MX_TIM1_Init(void)
 {
-	  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-	  TIM_MasterConfigTypeDef sMasterConfig = {0};
-	  TIM_OC_InitTypeDef sConfigOC = {0};
 
-	  htim1.Instance = TIM1;
-	  htim1.Init.Prescaler = 0;
-	  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-	  htim1.Init.Period = 65535;
-	  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
-	  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
-	  {
-		  Error_Handler();
-	  }
-	  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
+  HAL_TIM_MspPostInit(&htim1);
 
-	  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-
-	  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-	  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-
-	  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
-
-	  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-	  sConfigOC.Pulse = 0;
-	  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-	  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-	  if ((HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1)) != HAL_OK)
-	  {
-	    Error_Handler();
-	  }
 }
 
 void Error_Handler(void)
